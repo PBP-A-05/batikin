fc3569135955565571a7b2c7f3d315759792ab5c
denisfilab <daanish.inayat@ui.ac.id>
2024-10-09 20:22:50 +0700

    feat: update allowed host

--- batikin/settings.py
+++ batikin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-bmxnn0ux!saqzi00pey0^=af!d&q*n$_3k3zs6eo-=$+kh+9y2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['daanish-inayat-batikin.pbp.cs.ui.ac.id', '12', "localhost", "127.0.0.1"]
 
 
 # Application definition
