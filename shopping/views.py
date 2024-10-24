479f129f1c7340d772a6e3869edcabf3903f68fe
bizarrebeam <adelyaamanda@gmail.com>
2024-10-24 18:43:57 +0700

    solved different display thru navbar & filter button

--- shopping/views.py
+++ shopping/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.core import serializers
 import json
 from django.shortcuts import render
@@ -21,6 +21,17 @@ def product_detail(request, pk):  # Renamed parameter from 'product_id' to 'pk'
 def filter_products(request):
     category = request.GET.get('category')
     products = Product.objects.filter(category=category) if category else Product.objects.all()
-    data = serializers.serialize('json', products)
-    return HttpResponse(data, content_type='application/json')
+    data = [
+        {
+            'pk': str(product.pk),
+            'fields': {
+                'product_name': product.product_name,
+                'price': product.price,
+                'image_urls': product.image_urls,
+                'category': product.category,
+            }
+        }
+        for product in products
+    ]
+    return JsonResponse(data, safe=False)
 
