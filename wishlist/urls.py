74a6be8a3748ebee84e5e0d9811ef7353a38804b
nawaetuna <nawaetuna@gmail.com>
2024-10-26 20:40:46 +0700

    update

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.wishlist_view, name='wishlist_view'),
     path('add/<uuid:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('remove_from_wishlist/<uuid:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+    path('cart/add/<uuid:product_id>/', views.add_to_cart, name='add_to_cart'),
 ]
