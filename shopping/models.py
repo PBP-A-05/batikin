92c06e1b6e7accfc7b1a2f1ce376ff512c4ca587
bizarrebeam <adelyaamanda@gmail.com>
2024-10-26 13:49:15 +0700

    add model Review

--- shopping/models.py
+++ shopping/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -50,3 +51,16 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'product')  
+
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.product.product_name}"
